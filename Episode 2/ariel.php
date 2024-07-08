440745dd20035eedf849cd22c46743251697515d
arielyeong <arielyeong0526@gmail.com>
2024-07-08 23:36:29 +0800

    Add folders

--- Episode 2/ariel.php
+++ Episode 2/ariel.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Episode 2 Start</title>
-    <link rel="stylesheet" href="css/episode2_style.css">
+    <link rel="stylesheet" href="../css/episode2_style.css">
 </head>
 
 <style>
