ce3b233e2bd6810eb83bff6a2e5993e164b2fe95
huinannn <huinan26@gmail.com>
2024-08-10 21:43:17 +0800

    Update sql

--- sql/sdp_ga.sql
+++ sql/sdp_ga.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 31, 2024 at 03:35 PM
+-- Generation Time: Aug 10, 2024 at 03:40 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -20,6 +20,77 @@ SET time_zone = "+00:00";
 --
 -- Database: `sdp_ga`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `certificate_information`
+--
+
+CREATE TABLE `certificate_information` (
+  `CERTIFICATE_ID` int(11) NOT NULL,
+  `CERTIFICATE_TEMPLATE` varchar(255) NOT NULL,
+  `USER_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `episode`
+--
+
+CREATE TABLE `episode` (
+  `EPISODE_ID` int(11) NOT NULL,
+  `EPISODE_NAME` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `episode_result`
+--
+
+CREATE TABLE `episode_result` (
+  `EPISODE_RESULT_ID` int(11) NOT NULL,
+  `TIME_TAKEN` timestamp NOT NULL DEFAULT current_timestamp(),
+  `SCORE` int(11) NOT NULL,
+  `EPISODE_ID` int(11) NOT NULL,
+  `USER_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `game_episode`
+--
+
+CREATE TABLE `game_episode` (
+  `EPISODE_QUESTION_ID` int(11) NOT NULL,
+  `EPISODE_QUESTION` varchar(255) NOT NULL,
+  `EPISODE_HINT` varchar(255) NOT NULL,
+  `OPTION_A` varchar(255) NOT NULL,
+  `OPTION_A_EXPLANATION` varchar(255) NOT NULL,
+  `OPTION_B` varchar(255) NOT NULL,
+  `OPTION_B_EXPLANATION` varchar(255) NOT NULL,
+  `OPTION_C` varchar(255) NOT NULL,
+  `OPTION_C_EXPLANATION` varchar(255) NOT NULL,
+  `OPTION_D` varchar(255) NOT NULL,
+  `OPTION_D_EXPLANATION` varchar(255) NOT NULL,
+  `CORRECT_ANSWER` varchar(255) NOT NULL,
+  `EPISODE_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `score_information`
+--
+
+CREATE TABLE `score_information` (
+  `SCORE_ID` int(11) NOT NULL,
+  `EPISODE_TOTAL_SCORE` int(11) NOT NULL,
+  `USER_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -42,6 +113,36 @@ CREATE TABLE `user_information` (
 --
 
 --
+-- Indexes for table `certificate_information`
+--
+ALTER TABLE `certificate_information`
+  ADD PRIMARY KEY (`CERTIFICATE_ID`);
+
+--
+-- Indexes for table `episode`
+--
+ALTER TABLE `episode`
+  ADD PRIMARY KEY (`EPISODE_ID`);
+
+--
+-- Indexes for table `episode_result`
+--
+ALTER TABLE `episode_result`
+  ADD PRIMARY KEY (`EPISODE_RESULT_ID`);
+
+--
+-- Indexes for table `game_episode`
+--
+ALTER TABLE `game_episode`
+  ADD PRIMARY KEY (`EPISODE_QUESTION_ID`);
+
+--
+-- Indexes for table `score_information`
+--
+ALTER TABLE `score_information`
+  ADD PRIMARY KEY (`SCORE_ID`);
+
+--
 -- Indexes for table `user_information`
 --
 ALTER TABLE `user_information`
@@ -52,10 +153,40 @@ ALTER TABLE `user_information`
 --
 
 --
+-- AUTO_INCREMENT for table `certificate_information`
+--
+ALTER TABLE `certificate_information`
+  MODIFY `CERTIFICATE_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `episode`
+--
+ALTER TABLE `episode`
+  MODIFY `EPISODE_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `episode_result`
+--
+ALTER TABLE `episode_result`
+  MODIFY `EPISODE_RESULT_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `game_episode`
+--
+ALTER TABLE `game_episode`
+  MODIFY `EPISODE_QUESTION_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `score_information`
+--
+ALTER TABLE `score_information`
+  MODIFY `SCORE_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `user_information`
 --
 ALTER TABLE `user_information`
-  MODIFY `USER_ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `USER_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
