483874c034184bce5b167c547f6be99010042c24
EricTan1410 <TP071035@mail.apu.edu.my>
2024-08-14 12:36:01 +0800

    ep1

--- Episode 1/corrent1.php
+++ Episode 1/corrent1.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Episode</title>
-    <link rel="stylesheet" href="cor.css">
+    <link rel="stylesheet" href="../css/corrent1.css/">
     
 </head>
 <body>
