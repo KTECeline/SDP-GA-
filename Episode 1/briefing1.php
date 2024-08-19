8914a346439a7941b030cc448779a08ca02751f8
Khaw Tze Ern,Celine <157878271+KTECeline@users.noreply.github.com>
2024-08-20 02:13:38 +0800

    css

--- Episode 1/briefing1.php
+++ Episode 1/briefing1.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Episode 1 Brief</title>
-    <link rel="stylesheet" href="css/brief.css">
+    <link rel="stylesheet" href="../css/brief.css">
     
 
 <!-- Google link font -->
