cfdade20250403560a3dfd84173f57483188d979
huinannn <huinan26@gmail.com>
2024-08-21 19:41:28 +0800

    update admin profile

--- admin/profile/update.php
+++ admin/profile/update.php
@@ -2,31 +2,45 @@
     include '../../conn/conn.php';
 
     if (isset($_POST['submit'])) { 
-        $id = $_POST['id'];
-        $name = $_POST['new_name'];
-        $email = $_POST['new_email'];
-        $phone = $_POST['new_phonenum'];
-        $username = $_POST['new_username'];
-        $password = $_POST['new_password'];
+        $id = mysqli_real_escape_string($dbConn, $_POST['id']);
+        $name = mysqli_real_escape_string($dbConn, $_POST['new_name']);
+        $email = mysqli_real_escape_string($dbConn, $_POST['new_email']);
+        $phone = mysqli_real_escape_string($dbConn, $_POST['new_phonenum']);
+        $username = mysqli_real_escape_string($dbConn, $_POST['new_username']);
+        $password =  mysqli_real_escape_string($dbConn, $_POST['new_password']);
 
-        $sql = "UPDATE user_information SET " .
-                "USER_NAME = '$name', " .  
-                "USER_EMAIL = '$email', " .
-                "USER_PHONENUMBER= '$phone'," .
-                "USER_USERNAME = '$username', " .
-                "USER_PASSWORD = '$password' " .
-                "WHERE USER_ID = $id";
+        $emailCheck = mysqli_query($dbConn, "SELECT * FROM user_information WHERE USER_EMAIL = '$email' AND USER_ID != $id");
 
-        mysqli_query($dbConn, $sql);
+        $usernameCheck = mysqli_query($dbConn, "SELECT * FROM user_information WHERE USER_USERNAME = '$username' AND USER_ID != $id");
 
-        if (mysqli_affected_rows($dbConn) > 0) { 
-            echo "<script>alert('Successfully update data!');</script>";
+        if (mysqli_num_rows($emailCheck) > 0) { 
+            echo "<script>alert('Email already exists. Please use another email.');</script>";
             echo "<script>window.location.href='../../admin/profile/profile.php';</script>";
-            exit(); 
-        } else {
-            echo "<script>alert('Cannot update data!');</script>";
-            echo "<script>window.location.href='../../../admin/profile/ediprofile.php';</script>";
             exit();
+        } elseif (mysqli_num_rows($usernameCheck) > 0) { 
+            echo "<script>alert('Username already exists. Please use another username.');</script>";
+            echo "<script>window.location.href='../../admin/profile/profile.php';</script>";
+            exit();
+        } else {
+            $sql = "UPDATE user_information SET " .
+                    "USER_NAME = '$name', " .  
+                    "USER_EMAIL = '$email', " .
+                    "USER_PHONENUMBER= '$phone'," .
+                    "USER_USERNAME = '$username', " .
+                    "USER_PASSWORD = '$password' " .
+                    "WHERE USER_ID = $id";
+
+            mysqli_query($dbConn, $sql);
+
+            if (mysqli_affected_rows($dbConn) > 0) { 
+                echo "<script>alert('Successfully updated data!');</script>";
+                echo "<script>window.location.href='../../admin/profile/profile.php';</script>";
+                exit(); 
+            } else {
+                echo "<script>alert('Cannot update data!');</script>";
+                echo "<script>window.location.href='../../admin/profile/profile.php';</script>";
+                exit();
+            }
         }
     } else {
         echo "<script>window.location.href='../../admin/profile/profile.php';</script>";
